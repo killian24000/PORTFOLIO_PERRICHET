04953de01055877bc2c5a49d571991a142d54761
Killian PERRICHET <perrichetkillian@gmail.com>
2023-03-30 15:19:54 +0200

    Update Neon

--- public/header.php
+++ public/header.php
@@ -2,10 +2,10 @@
       <header class="container-header">
         <div>
           <p>
-          <h1 class="txt-al-center">PERRICHET KILLIAN</h1>
+          <h1 class="txt-al-center NeonEffect">PERRICHET KILLIAN</h1>
           </p>
           <p>
-          <h1 class="txt-al-center">DEVELOPPEUR WEB</h1>
+          <h1 class="txt-al-center NeonEffect">DEVELOPPEUR WEB</h1>
           </p>
         </div>
       </header>
